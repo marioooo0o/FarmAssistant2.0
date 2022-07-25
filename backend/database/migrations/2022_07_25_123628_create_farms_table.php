039b2ddee9ffc0812491b2fabcfd2424bfe0d820
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-07-25 14:54:39 +0200

    feat: create farms migration file and empty farm controller

---
+++ backend/database/migrations/2022_07_25_123628_create_farms_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 1024);
+            $table->string('street', 1024);
+            $table->unsignedInteger('street_number');
+            $table->string('postal_code',6);
+            $table->string('city', 1024);
+            $table->float('area')->default(0.00);
+            $table->timestamps();
+            $table->foreign('user_id')
+                        ->references('id')
+                        ->on('users')
+                        ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('farms');
+    }
+};
