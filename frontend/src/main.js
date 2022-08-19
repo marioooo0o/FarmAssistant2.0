4fc5d667cc749f64ea641e473180526c30a7e676
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-08-19 23:12:32 +0200

    feat: create global components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BaseButton from './components/ui/BaseButton.vue'
+import BaseCard from './components/ui/BaseCard.vue'
+import BaseDescriptionCard from './components/ui/BaseDescriptionCard.vue'
+import BaseInput from './components/ui/BaseInput.vue'
 import router from './router'
 
 import './assets/main.css'
@@ -7,5 +11,11 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(router)
+app.config.unwrapInjectedRef = true;
+
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
+app.component('base-description-card', BaseDescriptionCard);
+app.component('base-input', BaseInput);
 
 app.mount('#app')
