bd17fa79a0d5a25a1c8507c789e288cebccb7f8a
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-08-20 20:39:59 +0200

    feat: bind data in Field List Item Component

--- frontend/src/components/fields/FieldListItem.vue
+++ frontend/src/components/fields/FieldListItem.vue
@@ -1,20 +1,24 @@
 <template>
     <div
         class="bg-white solid border-[1px] font-semibold border-black rounded-lg grid grid-cols-4 justify-center items-center text-xs box-border m-3 tracking-wider p-1 hover:ring-4 cursor-pointer">
-        <div class="flex justify-center items-center">Pole Romana</div>
-        <div class="flex justify-center items-center">11,00 ha</div>
+        <div class="flex justify-center items-center">{{ field.name }}</div>
+        <div class="flex justify-center items-center">{{field.area}} ha</div>
         <div class="flex justify-center items-center">
             <ul>
-                <li>Działka 234</li>
-                <li>Działka 247</li>
+                <li v-for="parcel in field.parcels" :key="field.parcels.id">Działka {{parcel.name}}</li>
             </ul>
         </div>
-        <div class="flex justify-center items-center"><img src="@/assets/crops/tomato.png" alt="tomato" height="50px" width="50px" class="w-5"></div>
+        <div class="flex justify-center items-center"><img :src="field.crop.src" :alt=field.crop.alt height="50px" width="50px" class="w-5"></div>
     </div>
 </template>
 <script>
 export default {
-
+    props:{
+        field:{
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
 <style>
